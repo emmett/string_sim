b273039d4bd0bd6188e0455c4b338c44a605f3f7
Emmett Faricy <Emmett@mixpanel.com>
2020-01-17 11:41:08 -0700

    Handled empty list

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # This call to setup() does all the work
 setup(
     name="mp-string-similarity",
-    version="2.0.0",
+    version="2.1.0",
     description="MP string similarity check",
     license="MIT",
     classifiers=[
